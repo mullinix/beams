d82b2b2f92d6c6f9ac839fbd3b78ce6b27ea432e
= <=>
2015-03-04 10:53:36 -0800

    restoring more files that were lost.

---
+++ evec_perm.m
@@ -0,0 +1,13 @@
+mode = 9;
+% evec_test = shape(:,mode);
+% len = length(evals)/6;
+% II = eye(len);
+% ZZ = zeros(len);
+% Rho = [ZZ,ZZ,II;
+%        II,ZZ,ZZ;
+%        ZZ,II,ZZ];% S3 permutation matrix
+% perm = 2;
+% spy(Rho^perm)
+% evec_perm1 = (Rho^(perm))*evec_test;
+% res = (X2*evals(mode)^2-X1*evals(mode)-X0)*evec_perm1;
+% norm(res)
